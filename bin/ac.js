788233ae4d90118fb976e7ca806cf26f58b2f128
alianthes <ali@mapbox.com>
2015-09-19 22:32:02 -0400

    map view calculator

--- bin/ac.js
+++ bin/ac.js
@@ -4,4 +4,9 @@ var arguments = require("minimist")(process.argv.slice(2));
 var functionType = arguments["_"][0];
 var number = arguments["_"][1];
 
+var ac = module.exports = {};
 
+ac.costView = function(views) {
+	var cost = ((views/1000) * 0.50);
+	return cost;
+};
